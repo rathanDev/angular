5f0c6db1e85a02738567403e2f1715f65f5fd419
Janarthan <janarthanp@hsenidmobile.com>
2017-05-30 20:16:49 +0530

    Added File System module and read a file

--- server/example.js
+++ server/example.js
@@ -4,11 +4,22 @@ const port = 3000;
 
 var dateTime = require('./MyFirstModule');
 
+var fs = require('fs');
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.write("Date and Time : " + dateTime.myDateTime());
-    res.end();
+
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.write("Date and Time : " + dateTime.myDateTime() + "\n");
+    // res.write(req.url);
+    // res.end();
+
+    fs.readFile('/home/jana/myProjects/angular/server/test.txt', (err, data) => {
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+        res.end("read file");
+    });
+
 })
 
 server.listen(port, hostname, () => {
