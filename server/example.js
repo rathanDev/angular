ae0bd88f6ed80824e16c5ca758df8727e1ccd23c
Janarthan <janarthanp@hsenidmobile.com>
2017-05-30 19:41:13 +0530

    Added own module

--- server/example.js
+++ server/example.js
@@ -1,13 +1,16 @@
-const http = require('http')
-const hostname = '127.0.0.1'
-const port = 3000
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-const server = http.createServer((req, res) = > {
+var dateTime = require('./MyFirstModule');
+
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World \n');
+    res.write("Date and Time : " + dateTime.myDateTime());
+    res.end();
 })
 
-server.listen(port, hostname, () = > {
+server.listen(port, hostname, () => {
     console.log('Server running at http://${hostname}:${port}/');
-})
+});
