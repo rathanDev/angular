f630d5b48086c8b13fdf621d067cac607fc2795f
jana <rathan.kparam@gmail.com>
2017-07-29 11:46:30 +0530

    Added navigation

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -5,21 +5,39 @@ import {AppComponent} from './app.component';
 import {HeroDetailComponent} from "./hero-detail.component";
 import {HeroesComponent} from "./heroes.component";
 import {HeroService} from "./hero.service";
+import {RouterModule} from '@angular/router';
+import {DashboardComponent} from "./dashboard.component";
 
 @NgModule({
-    imports: [
-      BrowserModule,
-      FormsModule
-    ],
-    declarations: [
-        AppComponent,
-        HeroDetailComponent,
-        HeroesComponent
-    ],
-    providers: [
-      HeroService
-    ],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
 })
 
 export class AppModule {
