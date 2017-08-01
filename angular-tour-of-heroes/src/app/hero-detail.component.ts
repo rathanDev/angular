66e38039d0654e238250226d57071e8aac614bab
jana <rathan.kparam@gmail.com>
2017-08-01 21:40:19 +0530

    Style the navigation links

--- angular-tour-of-heroes/src/app/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail.component.ts
@@ -7,7 +7,8 @@ import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
