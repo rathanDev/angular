dd86416625256b5090a9cde3083f6987bb4bf404
janarthan <janarthan@finnovation.lk>
2017-11-25 19:10:13 +0530

    Update of service worker

---
+++ ngApp/src/sw.js
@@ -0,0 +1,32 @@
+const VERSION = 'v1';
+
+function log(messages) {
+  console.log(VERSION, messages);
+}
+
+// log("Installing Service Worker");
+
+//life cycle events
+
+self.addEventListener('install', () => {
+  log('installed');
+  onServiceWorkerInstalled();
+});
+
+self.addEventListener('activate', () => {
+  log('activated')
+});
+
+async function onServiceWorkerInstalled() {
+
+  const request = new Request('https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png');
+  const response = await fetch(request);
+  log("Response received", response);
+  if(response.status !== 200) {
+    throw new Error("Could not load offline content");
+  }
+
+  const cache = await caches.open('app-cache');
+  cache.put(request, response);
+  log("Cached offline content")
+}
