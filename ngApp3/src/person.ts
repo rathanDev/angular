83b13442fd8655f2365440d3cfc4e3fb7995d279
jana <rathan.kparam@gmail.com>
2017-11-29 05:51:41 +0530

    Add sample ts class

---
+++ ngApp3/src/person.ts
@@ -0,0 +1,27 @@
+export class Person {
+
+    private id: number;
+    private name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    setId(id: number) {
+        this.id = id;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name: string) {
+        this.name = name;
+    }
+
+}
