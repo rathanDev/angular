ff6e8a0e90d300bb1b98c103afc6fbbd1d779fb0
janarthan <janarthan@finnovation.lk>
2018-03-22 17:53:42 +0530

    Add 2 way data binding

--- my-ap/src/app/app.module.ts
+++ my-ap/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-ap/src/app/heroes/heroes.component.ts
+++ my-ap/src/app/heroes/heroes.component.ts
@@ -8,7 +8,7 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero : Hero = {
+  hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
