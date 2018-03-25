3f6e2df2bdddc6fac60a28a18858f376b9cc7933
janarthan <janarthan@finnovation.lk>
2018-03-25 17:40:02 +0530

    Try @Input

--- ngBk2Ang4/hello-world/src/app/user-item/user-item.component.ts
+++ ngBk2Ang4/hello-world/src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -7,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserItemComponent implements OnInit {
 
-  name: string;
+  @Input() name: string;
 
   constructor() {
-    this.name = 'Felipe';
   }
 
   ngOnInit() {
