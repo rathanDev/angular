3c353f314ab407e1297ae34f0be88f99197400b6
janarthan <janarthan@finnovation.lk>
2018-03-25 17:48:29 +0530

    Try upto page30

--- ngBk2Ang4/hello-world/src/app/app.module.ts
+++ ngBk2Ang4/hello-world/src/app/app.module.ts
@@ -1,24 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { UserItemComponent } from './user-item/user-item.component';
-import { UserListComponent } from './user-list/user-list.component';
-
+import {AppComponent} from './app.component';
+import {HelloComponent} from './hello/hello.component';
+import {UserItemComponent} from './user-item/user-item.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
-    UserItemComponent,
-    UserListComponent
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
