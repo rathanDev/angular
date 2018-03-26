c2a4ad84dde08585368414b6fe09863fae5fac03
janarthan <janarthan@finnovation.lk>
2018-03-26 15:01:15 +0530

    Add es6 arrow function to sort article

--- ngBk2Ang4/angular-reddit/src/app/app.component.ts
+++ ngBk2Ang4/angular-reddit/src/app/app.component.ts
@@ -17,4 +17,16 @@ export class AppComponent {
     ]
   }
 
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log("add article");
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
 }
