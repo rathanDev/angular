5e9195e2e7095fd50512f9507c9eba32034a0253
Jana <rathan.kparam@gmail.com>
2018-04-13 09:55:15 +0530

    Update

--- ngBk2Ang4/http2/src/app/app.module.ts
+++ ngBk2Ang4/http2/src/app/app.module.ts
@@ -1,12 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
-import { SimpleHttpComponent } from './simple-http/simple-http.component';
+import {AppComponent} from './app.component';
+import {SimpleHttpComponent} from './simple-http/simple-http.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {youTubeSearchInjectables} from './you-tube-search.injectables';
-
 
 @NgModule({
   declarations: [
@@ -20,4 +18,5 @@ import {youTubeSearchInjectables} from './you-tube-search.injectables';
   providers: [HttpClient, youTubeSearchInjectables],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- ngBk2Ang4/http2/src/app/you-tube-search.service.ts
+++ ngBk2Ang4/http2/src/app/you-tube-search.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {SearchResult} from './search-result.model';
+import {HttpClient} from '@angular/common/http';
 
 export const YOUTUBE_API_KEY = '';
 export const YOUTUBE_API_URL = '"https://www.googleapis.com/youtube/v3/search';
@@ -6,6 +9,42 @@ export const YOUTUBE_API_URL = '"https://www.googleapis.com/youtube/v3/search';
 @Injectable()
 export class YouTubeSearchService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+              @Inject(YOUTUBE_API_KEY) private apiKey: string,
+              @Inject(YOUTUBE_API_URL) private apiUrl: string) {
+  }
+
+  // search(query: string): Observable<SearchResult[]> {
+  search(query: string) {
+    const params: string = [
+      `q=${query}`,
+      `key=${this.apiKey}`,
+      `part=snippet`,
+      `type=video`,
+      `maxResults=10`
+    ].join('&');
+    const queryUrl = `${this.apiUrl}?${params}`;
+
+    return this.http.get(queryUrl)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+
+  /*
+  return this.http.get(queryUrl)
+.map((response: Response) => {
+return (<any>response.json()).items.map(item => {
+// console.log("raw item", item); // uncomment if you want to debug
+return new SearchResult({
+id: item.id.videoId,
+title: item.snippet.title,
+description: item.snippet.description,
+thumbnailUrl: item.snippet.thumbnails.high.url
+});
+});
+});
+  * */
 
 }
