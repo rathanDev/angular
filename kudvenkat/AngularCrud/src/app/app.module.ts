f23bc72f0947eb7bfdd83cb3f897fcdead388860
janarthan <janarthan@finnovation.lk>
2018-04-16 18:06:10 +0530

    Use router

--- kudvenkat/AngularCrud/src/app/app.module.ts
+++ kudvenkat/AngularCrud/src/app/app.module.ts
@@ -3,14 +3,24 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {EmployeeListComponent} from './employee/employee-list/employee-list.component';
+import {EmployeeCreationComponent} from './employee/employee-creation/employee-creation.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'list', component: EmployeeListComponent},
+  {path: 'create', component: EmployeeCreationComponent},
+  {path: '', redirectTo: '/list', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    EmployeeCreationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ kudvenkat/AngularCrud/src/app/employee/employee-creation/employee-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-creation',
+  templateUrl: './employee-creation.component.html',
+  styleUrls: ['./employee-creation.component.css']
+})
+export class EmployeeCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
