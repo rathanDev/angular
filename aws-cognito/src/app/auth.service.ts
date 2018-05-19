6a6f8fd1c60b714c8ea136825f8497c724a8527e
Jana <rathan.kparam@gmail.com>
2018-05-19 11:48:50 +0530

    Add aws-sdk

--- aws-cognito/src/app/auth.service.ts
+++ aws-cognito/src/app/auth.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import {Registration} from './sign-up/registration.model';
 
+// userPool
+const poolData = {
+  UserPoolId: 'us-east-1_nT09q7Hv8',
+  ClientId: '3bsakfglbeg3qgid8pjj1pjfth' // angularWebApp
+};
+
 @Injectable()
 export class AuthService {
 
   constructor() { }
 
   signUp(registration: Registration) {
-    console.log('username', registration.username, ' password', registration.password)
+    console.log('In authService username', registration.username, ' password', registration.password);
   }
 
 }
