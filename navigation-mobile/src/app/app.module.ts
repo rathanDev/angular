a4ec05738843036320bb1e9c0b906857325e068f
Jana <rathan.kparam@gmail.com>
2018-05-26 15:00:33 +0530

    Add basic components

--- navigation-mobile/src/app/app.module.ts
+++ navigation-mobile/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing/routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    Page1Component,
+    Page2Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- navigation-mobile/src/app/routing/routing.module.ts
+++ navigation-mobile/src/app/routing/routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from '../home/home.component';
+import {Page1Component} from '../page1/page1.component';
+import {Page2Component} from '../page2/page2.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'page1', component: Page1Component},
+  {path: 'page2', component: Page2Component}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   declarations: []
 })
