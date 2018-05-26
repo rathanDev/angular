b27ff91910b02d5177110187f32d97b95f37d4af
Jana <rathan.kparam@gmail.com>
2018-05-26 14:39:14 +0530

    Init

---
+++ navigation-mobile/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { RoutingModule } from './routing/routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
