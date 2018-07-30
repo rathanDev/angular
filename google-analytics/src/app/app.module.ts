569f8529156e573ad81c0d4ed06246b39e7c4b85
janarthan <janarthan@finnovation.lk>
2018-07-30 10:33:07 +0530

    Add faq and help components

--- google-analytics/src/app/app.module.ts
+++ google-analytics/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelpComponent } from './help/help.component';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelpComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule
