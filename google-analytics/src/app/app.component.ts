c0a8d33fd8098444ac7c6dd0367631d33441374e
janarthan <janarthan@finnovation.lk>
2018-07-30 12:16:38 +0530

    Add pageViews with url it redirects

--- google-analytics/src/app/app.component.ts
+++ google-analytics/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,14 @@ import {Component} from '@angular/core';
 export class AppComponent {
 
   title = 'Google Analytics - Angular App';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).ga('set', 'page', event.urlAfterRedirects);
+        (<any>window).ga('send', 'pageView');
+      }
+    });
+  }
+
 }
