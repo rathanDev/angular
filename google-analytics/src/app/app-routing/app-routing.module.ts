e2f43ac6be8ddb5d8e59f89f1ed8076dba428768
janarthan <janarthan@finnovation.lk>
2018-07-30 11:08:14 +0530

    Add routes

--- google-analytics/src/app/app-routing/app-routing.module.ts
+++ google-analytics/src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {HelpComponent} from '../help/help.component';
+import {FaqComponent} from '../faq/faq.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'help', component: HelpComponent},
+  {path: 'faq', component: FaqComponent}
+];
 
 @NgModule({
   imports: [
     CommonModule
   ],
+  exports: [
+    RouterModule
+  ],
   declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
