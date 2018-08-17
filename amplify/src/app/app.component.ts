fa2404da4087c037f1f8641818b481c13344b2a6
janarthan <janarthan@finnovation.lk>
2018-08-17 16:01:06 +0530

    Implement OnInit and OnDestroy

--- amplify/src/app/app.component.ts
+++ amplify/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
+
+  constructor(private auth: AuthService) {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  ngOnInit(): void {
+  }
 }
