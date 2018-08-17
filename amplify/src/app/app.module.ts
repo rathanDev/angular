2dd96d70e53763dc24411452e26d8699360507da
janarthan <janarthan@finnovation.lk>
2018-08-17 15:12:54 +0530

    Add auth service

--- amplify/src/app/app.module.ts
+++ amplify/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -10,7 +9,6 @@ import { RegistrationComponent } from './registration/registration.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
     HomeComponent,
     LoginComponent,
     RegistrationComponent
