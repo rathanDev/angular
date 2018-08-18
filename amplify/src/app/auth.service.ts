1064d326f82ac091833d9f3da09598404d7d2a5d
Jana <rathan.kparam@gmail.com>
2018-08-18 10:03:26 +0530

    Implement auth methods using amplify.Auth

--- amplify/src/app/auth.service.ts
+++ amplify/src/app/auth.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable, of} from 'rxjs';
 import {Router} from '@angular/router';
-import Amplify from 'aws-amplify';
+import Amplify, {Auth} from 'aws-amplify';
 import {environment} from '../environments/environment';
+import {fromPromise} from 'rxjs/internal/observable/fromPromise';
+import {catchError, map, tap} from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,49 @@ export class AuthService {
 
   constructor(private router: Router) {
     Amplify.configure(environment.amplify);
+    this.loggedIn = new BehaviorSubject<boolean>(false);
+  }
+
+  public signUp(email, password): Observable<any> {
+    return fromPromise(Auth.signUp(email, password));
+  }
+
+  public confirmSignUp(email, code): Observable<any> {
+    return fromPromise(Auth.confirmSignUp(email, code));
+  }
+
+  public login(email, password): Observable<any> {
+    return fromPromise(Auth.signIn(email, password))
+      .pipe(
+        tap(() => this.loggedIn.next(true))
+      );
+  }
+
+  public isAuthenticated(): Observable<boolean> {
+    return fromPromise(Auth.currentAuthenticatedUser())
+      .pipe(
+        map(result => {
+          this.loggedIn.next(true);
+          return true;
+        }),
+        catchError(error => {
+          this.loggedIn.next(false);
+          return of(false);
+        })
+      );
+  }
+
+  public signOut() {
+    fromPromise(Auth.signOut())
+      .subscribe(
+        res => {
+          this.loggedIn.next(false);
+          this.router.navigate(['/login']);
+        },
+        err => {
+          console.error('err', err);
+        }
+      );
   }
 
 }
