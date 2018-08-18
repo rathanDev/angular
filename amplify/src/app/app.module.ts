880d25738939867a9b1f0538734a33b0ba41666c
Jana <rathan.kparam@gmail.com>
2018-08-18 16:20:50 +0530

    Import FormsModule

--- amplify/src/app/app.module.ts
+++ amplify/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import {AppRoutingModule} from './app-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
