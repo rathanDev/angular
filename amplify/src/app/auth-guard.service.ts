63d04eded0adc8017a617c12b3a96fb637277767
Jana <rathan.kparam@gmail.com>
2018-08-18 11:19:00 +0530

    Implement canActivate with isAuthenticated

--- amplify/src/app/auth-guard.service.ts
+++ amplify/src/app/auth-guard.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
 import {AuthService} from './auth.service';
+import {tap} from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,14 @@ export class AuthGuardService implements CanActivate {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return null;
+    return this.auth.isAuthenticated()
+      .pipe(
+        tap(loggedIn => {
+          if (!loggedIn) {
+            this.router.navigate(['/login']);
+          }
+        })
+      );
   }
 
 }
