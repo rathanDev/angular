98791c5504bfdbfa01f90807fa235c57e06c40ee
janarthan <janarthan@finnovation.lk>
2018-08-23 13:36:58 +0530

    Refactor

--- amplify/src/app/auth.service.ts
+++ amplify/src/app/auth.service.ts
@@ -26,16 +26,8 @@ export class AuthService {
 
     // public access
     Amplify.configure({
-      Auth: {
-        identityPoolId: 'us-east-1:fb232da5-18c8-43c9-a380-27f2ed6a4345',
-        region: 'us-east-1',
-        userPoolId: 'us-east-1_jPBiZJltm',
-        userPoolWebClientId: '4b5mel5kbfe66emskjfoj64gpl'
-      },
-      Storage: {
-        bucket: 'test-aug-bucket',
-        region: 'us-east-1'
-      }
+      Auth: environment.amplify.auth,
+      Storage: environment.amplify.storage
     });
 
     // private access
--- amplify/src/environments/environment.ts
+++ amplify/src/environments/environment.ts
@@ -1,5 +1,13 @@
 const region = 'us-east-1';
 
+const augBucket = 'test-aug-bucket';
+
+const augPool = {
+  userPoolId: 'us-east-1_jPBiZJltm',
+  appClientId: '4b5mel5kbfe66emskjfoj64gpl',
+  identityPoolId: 'us-east-1:fb232da5-18c8-43c9-a380-27f2ed6a4345'
+};
+
 export const environment = {
 
   production: false,
@@ -9,14 +17,14 @@ export const environment = {
     region: 'us-east-1',
 
     auth: {
-      identityPoolId: 'us-east-1:fb232da5-18c8-43c9-a380-27f2ed6a4345',
+      identityPoolId: augPool.identityPoolId,
       region: region,
-      userPoolId: 'us-east-1_jPBiZJltm',
-      userPoolWebClientId: '4b5mel5kbfe66emskjfoj64gpl'
+      userPoolId: augPool.userPoolId,
+      userPoolWebClientId: augPool.appClientId
     },
 
     storage: {
-      bucket: 'test-aug-bucket',
+      bucket: augBucket,
       region: region
     }
 
