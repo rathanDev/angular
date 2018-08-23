e908bf915a1dcb4b78cfb1f57a60e47c0fa5e722
janarthan <janarthan@finnovation.lk>
2018-08-23 13:18:50 +0530

    Refactor

--- amplify/src/environments/environment.ts
+++ amplify/src/environments/environment.ts
@@ -1,29 +1,25 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+const region = 'us-east-1';
 
 export const environment = {
+
   production: false,
+
   amplify: {
-    Auth: {
-      angularPool: {
-        identityPoolId: 'us-east-1:7ea8f079-9171-4563-bac7-fb580bc96a50',
-        region: 'us-east-1',
-        userPoolId: 'us-east-1_QgX2kI3Lp',
-        userPoolWebClientId: '30mqqhd49kel6033ua0mvsk4cq'
-      }
+
+    region: 'us-east-1',
+
+    auth: {
+      identityPoolId: 'us-east-1:fb232da5-18c8-43c9-a380-27f2ed6a4345',
+      region: region,
+      userPoolId: 'us-east-1_jPBiZJltm',
+      userPoolWebClientId: '4b5mel5kbfe66emskjfoj64gpl'
     },
+
     storage: {
       bucket: 'test-aug-bucket',
-      region: 'us-east-1'
+      region: region
     }
-  }
-};
 
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  }
+
+};
