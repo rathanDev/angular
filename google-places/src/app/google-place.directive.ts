601c3ff06aebb31125c336313896234db84b3ce8
Jana <rathan.kparam@gmail.com>
2018-08-26 22:27:19 +0530

    Print address lat and lng

--- google-places/src/app/google-place.directive.ts
+++ google-places/src/app/google-place.directive.ts
@@ -15,6 +15,19 @@ export class GooglePlaceDirective implements OnInit {
   ngOnInit(): void {
     const autoComplete = new google.maps.places.Autocomplete(this.element);
     console.log('autoComplete ', autoComplete);
+
+    google.maps.event.addListener(autoComplete, 'place_changed', () => {
+      console.log('this', this);
+      this.getFormattedAddress(autoComplete.getPlace());
+    });
+
+  }
+
+  getFormattedAddress(place) {
+    console.log('place ', place);
+    console.log('address ', place.formatted_address);
+    console.log('lat ', place.geometry.location.lat());
+    console.log('lng ', place.geometry.location.lng());
   }
 
 }
