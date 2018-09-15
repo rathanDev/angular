5f856c459466db7e563b12274cb4bdb2607f0900
jana <rathan.kparam@gmail.com>
2018-09-15 11:39:39 +0530

    Update create component

--- ngrx-store/src/app/create/create.component.ts
+++ ngrx-store/src/app/create/create.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppState} from '../app.state';
+import {Store} from '@ngrx/store';
+
+import * as TutorialActions from '../actions/tutorial.actions';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {
+  }
 
   ngOnInit() {
+  }
+
+  addTutorial(name, url) {
+    this.store.dispatch(new TutorialActions.AddTutorial({name: name, url: url}));
   }
 
 }
