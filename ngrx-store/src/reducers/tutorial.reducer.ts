9978d37181a9cd241edf2090fae3ea6e06ef6181
jana <rathan.kparam@gmail.com>
2018-09-15 11:09:47 +0530

    Add tutorial reducer

---
+++ ngrx-store/src/reducers/tutorial.reducer.ts
@@ -0,0 +1,20 @@
+import {Tutorial} from '../models/tutorial.model';
+import * as TutorialActions from '../actions/tutorial.actions';
+
+const initialState: Tutorial = {
+  name: 'Initial Tutorial',
+  url: 'https://google.com'
+};
+
+export function reducer(state: Tutorial[] = [initialState], action: TutorialActions.Actions) {
+
+  switch (action.type) {
+    case TutorialActions.ADD_TUTORIAL:
+      return [...state, action.payload];
+
+    default:
+      return state;
+
+  }
+
+}
