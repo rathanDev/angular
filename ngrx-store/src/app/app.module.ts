0dde597b1428c48bd5af9347200b47d179c73d8d
jana <rathan.kparam@gmail.com>
2018-09-15 11:27:25 +0530

    Create read and create components

--- ngrx-store/src/app/app.module.ts
+++ ngrx-store/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {StoreModule} from '@ngrx/store';
 
-import {reducer} from '../reducers/tutorial.reducer';
+import {reducer} from './reducers/tutorial.reducer';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
