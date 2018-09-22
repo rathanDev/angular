4dee0017abbbc61188c997c7db4888fb89ee6ae9
jana <rathan.kparam@gmail.com>
2018-09-22 14:43:51 +0530

    Add google sign in

--- aws-cognito/src/app/sign-in/sign-in.component.ts
+++ aws-cognito/src/app/sign-in/sign-in.component.ts
@@ -1,14 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {AuthService} from '../auth.service';
 import {User} from '../sign-up/user.model';
 import {LambdaService} from '../lambda.service';
+
+declare var gapi: any;
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent implements OnInit, AfterViewInit {
 
   title = 'Sign In';
   user = new User();
@@ -27,6 +29,48 @@ export class SignInComponent implements OnInit {
     )
   }
 
+  ngAfterViewInit(): void {
+    this.initGoogle();
+  }
+
+  public auth2: any;
+
+  public initGoogle() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '599921436503-j8a6d0fffg21uik8mq8l9oumchojm7ds.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin(document.getElementById('googleBtn'));
+    });
+  }
+
+  public attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser) => {
+        let profile = googleUser.getBasicProfile();
+        let idToken = googleUser.getAuthResponse().id_token;
+        console.log('Token || ' + idToken);
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+
+        this.token = idToken;
+
+
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+  }
+
+  googleSignOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
 
   callAuthLambda() {
     this.lambdaService.callAuthSepLambda(this.token).subscribe(
