4eae17c3798effbbaf8005dd0032cea0ac73c67b
Janarthan <rathan.kparam@gmail.com>
2018-09-26 17:34:45 +0530

    Navigate on click

--- component-interaction-with-router/src/app/app.component.ts
+++ component-interaction-with-router/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {MessengerService} from './messenger.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,14 @@ export class AppComponent {
 
   appsign: boolean;
 
-  constructor(messenger: MessengerService) {
+  constructor(messenger: MessengerService,
+              private router: Router) {
     messenger.toggleSubject.subscribe(
       res => {
         console.log('@App subject', res);
         this.appsign = res;
         console.log('@App apphome', this.appsign);
+        this.router.navigate(['/about']);
       }
     );
   }
