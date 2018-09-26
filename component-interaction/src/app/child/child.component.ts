83c74f28304ff4ae1775f83e3c4b53a79d32efa5
Janarthan <rathan.kparam@gmail.com>
2018-09-26 15:23:13 +0530

    Add child component

--- component-interaction/src/app/child/child.component.ts
+++ component-interaction/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
-  constructor() { }
+  @Input() name;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
