94c12886bc08093732bad4ddb3ffdd46aca69182
Janarthan <janarthan.paramanandam@ge.com>
2018-12-24 16:35:07 +0530

    Add unit test

--- jasmine-karma/README.md
+++ jasmine-karma/README.md
@@ -25,3 +25,6 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+Reference:
+https://medium.com/frontend-fun/angular-unit-testing-jasmine-karma-step-by-step-e3376d110ab4
--- jasmine-karma/src/app/contact/contact.component.spec.ts
+++ jasmine-karma/src/app/contact/contact.component.spec.ts
@@ -1,16 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ContactComponent } from './contact.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {BrowserModule, By} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DebugElement} from '@angular/core';
+import {ContactComponent} from './contact.component';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ContactComponent],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(ContactComponent);
+        component = fixture.componentInstance;
+        de = fixture.debugElement.query(By.css('form'));
+        el = de.nativeElement;
+      });
   }));
 
   beforeEach(() => {
@@ -19,7 +34,12 @@ describe('ContactComponent', () => {
     fixture.detectChanges();
   });
 
+  it('should have as text "contact page" ', function () {
+    expect(component.text).toEqual('Contact Page');
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
