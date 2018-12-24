9ffa8ea6228b75892ba71d569e5810894c3d086a
Janarthan <janarthan.paramanandam@ge.com>
2018-12-24 14:44:37 +0530

    Init

--- angular-unit-testing/src/app/app-routing.module.ts
+++ angular-unit-testing/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {FormComponent} from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'form', component: FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- angular-unit-testing/src/app/app.module.ts
+++ angular-unit-testing/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
