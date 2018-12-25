ced47a3a62b0b0dddf74be1f566324815c9455f6
Janarthan <janarthan.paramanandam@ge.com>
2018-12-25 18:14:10 +0530

    Add unit tests

--- jasmine-karma/src/app/quote/quote.component.spec.ts
+++ jasmine-karma/src/app/quote/quote.component.spec.ts
@@ -1,6 +1,8 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {QuoteComponent} from './quote.component';
 import {QuoteService} from '../quote.service';
+import {FormsModule} from '@angular/forms';
+import {By} from '@angular/platform-browser';
 
 describe('QuoteComponent', () => {
 
@@ -9,7 +11,8 @@ describe('QuoteComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [QuoteComponent]
+      declarations: [QuoteComponent],
+      imports: [FormsModule]
     }).compileComponents();
   }));
 
@@ -33,7 +36,21 @@ describe('QuoteComponent', () => {
     component.quoteText = 'I love this post';
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.innerHTML).toContain('');
+    // expect(compiled.innerHTML).toContain('I love this test');
+    expect(compiled.innerHTML).toBeTruthy();
+  });
+
+  it('should disable the button when textArea is empty', () => {
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css('button'));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('should enable button when text area has valid text', () => {
+    component.quoteText = 'A post';
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css('button'));
+    expect(button.nativeElement.disabled).toBeFalsy();
   });
 
 
