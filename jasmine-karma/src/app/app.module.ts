51da4709c6b835072aeed6880bff85a7a9a6a215
Janarthan <janarthan.paramanandam@ge.com>
2018-12-25 14:55:11 +0530

    Add user component and user service

--- jasmine-karma/src/app/app.module.ts
+++ jasmine-karma/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContactComponent } from './contact/contact.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ContactComponent} from './contact/contact.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {UserComponent} from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +21,5 @@ import {ReactiveFormsModule} from '@angular/forms';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
