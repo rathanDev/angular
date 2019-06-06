a081db6f1c57d859bd3f027fcad616403f8c7289
Jana <janarthan.paramanandam@ge.com>
2019-06-06 20:39:06 +0530

    Change marker of the location on button click

--- map/src/app/dashboard/dashboard.component.ts
+++ map/src/app/dashboard/dashboard.component.ts
@@ -8,13 +8,26 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 export class DashboardComponent implements OnInit, OnDestroy {
 
   title = 'Agm Experiment';
+  mapCenterLat = 51.678418;
+  mapCenterLng = 7.809007;
   lat = 51.678418;
   lng = 7.809007;
+
+  locChanged = false;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  changeLoc() {
+    this.locChanged = true;
+
+    this.mapCenterLat = 41.678418;
+    this.mapCenterLng = 6.809007;
+    this.lat = 41.678418;
+    this.lng = 6.809007;
   }
 
   ngOnDestroy(): void {
