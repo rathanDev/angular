369bb3c6a6bb28f9a48bf5ac41e88e1af7e9a2a8
janarthan <jana@email.com>
2019-11-11 15:47:22 +0530

    Add dashboard and profile modules

--- lazy-loading/draft.md
+++ lazy-loading/draft.md
@@ -1,1 +1,5 @@
+ng new lazy-loading --routing
+
 ng g module dashboard --route dashboard --module app.module
+
+ng g m profile --route profile --module app.module
--- lazy-loading/src/app/app-routing.module.ts
+++ lazy-loading/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
