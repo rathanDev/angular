dbafb9233c2d26fe915e3461ff3f0e8d970780ec
janarthan <jana@email.com>
2019-11-25 14:52:17 +0530

    Claim victory

--- service-worker/other/draft.md
+++ service-worker/other/draft.md
@@ -1,3 +1,30 @@
 ng new service-worker
 
 ng s --o
+
+
+# App Shell
+ng g appShell --clientProject=service-worker --universalProject=service-worker-server
+
+ng run service-worker:app-shell
+ng run service-worker:app-shell:production
+
+# Add service worker
+ng add @angular/pwa --project service-worker
+
+ng build --prod
+
+npm i -g http-server
+http-server -p 8080 -c-1 dist/service-worker
+
+
+
+
+
+
+
+
+
+References:
+https://angular.io/guide/service-worker-intro
+
--- service-worker/src/app/app.module.ts
+++ service-worker/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    RouterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
